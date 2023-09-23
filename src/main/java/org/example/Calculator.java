b7f4dfdb12763c0bca528cddd0a7d9793c94a4f0
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-23 18:25:57 +0200

    Add userChoice method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,8 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println(0/8);
+        userChoice();
+
+    }
+
+    public static void userChoice() {
+        System.out.println("Welche Rechenmethode möchten Sie verwenden?");
+        System.out.println("Addieren=1, Subtrahieren=2, Multiplizieren=3, Dividieren=4");
+        Scanner sc = new Scanner(System.in);
+        int userInputForOperation = sc.nextInt();
+
+        System.out.println("Bitte gebe Nummmer 1: ");
+        double userInputNum1 = sc.nextDouble();
+
+        System.out.println("Bitte gebe Nummmer 2: ");
+        double userInputNum2 = sc.nextDouble();
+        sc.close();
+        System.out.println("User input operation = " + userInputForOperation);
+        System.out.println("User input num1 = " + userInputNum1);
+        System.out.println("User inout num2 = " + userInputNum2);
+
     }
 
     public static double add(double number1, double number2) {

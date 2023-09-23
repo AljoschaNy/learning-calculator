e89ac0f0e18adb4f887f741804b6b2999e8df216
FabianMariusLink <fabian.marius.link@gmail.com>
2023-09-23 17:28:10 +0200

    Add CalculatorTest

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public static void main(String[] args) {
 
     }
+
+    public static double add(double number1, double number2) {
+        return number1+number2;
+    }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
+    @Test
+    void add_whenNumber1Is5AndNumber2Is6_thenReturn11 () {
+        //GIVEN
+        double number1 = 5;
+        double number2 = 6;
+        //WHEN
+        double actual = Calculator.add(number1, number2);
+        //THEN
+        double expect = 11;
+        assertEquals(expect, actual);
+    }
 }

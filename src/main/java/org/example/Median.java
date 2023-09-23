1e5f7407377c213f27c9e510119f891ee708ad03
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-23 20:07:45 +0200

    Create user array from inputs

--- src/main/java/org/example/Median.java
+++ src/main/java/org/example/Median.java
@@ -4,30 +4,29 @@ import java.util.Scanner;
 
 public class Median {
     public static void main(String[] args){
-        int userArrayLength = getArrayLength();
-        int[] userArray = getArray(userArrayLength);
+        Scanner sc = new Scanner(System.in);
+        int userArrayLength = getArrayLength(sc);
+        int[] userArray = getArray(userArrayLength,sc);
+        sc.close();
         System.out.println(userArray[1]);
+        System.out.println(userArray[2]);
+        System.out.println(userArray[3]);
     }
 
-    public static int getArrayLength(){
+    public static int getArrayLength(Scanner scTest){
         System.out.println("Wie viele Zahlen möchtest du eingeben?");
-        Scanner sc = new Scanner(System.in);
-        int arrayLength = sc.nextInt();
-        sc.close();
+        int arrayLength = scTest.nextInt();
         return arrayLength;
     }
 
-    public static int[] getArray(int arrayLength){
+    public static int[] getArray(int arrayLength,Scanner scTest){
         int[] numbersForMedian = new int[arrayLength];
         System.out.println("Gib bitte die Zahlen ein!");
-        //Scanner sc = new Scanner(System.in);
-       // for(int i = 0; i < arrayLength; i++){
-            System.out.println("Zahl " + (1+1));
-            Scanner sc2 = new Scanner(System.in);
-            int num = sc2.nextInt();
-            numbersForMedian[0] = num;
-            sc2.close();
-        //}
+
+        for(int i = 0; i < arrayLength; i++){
+            System.out.println("Zahl " + (i+1));
+            numbersForMedian[i] = scTest.nextInt();
+        }
         return numbersForMedian;
     }
 }
